aba99de530650532345bb6d703529f59824efdc6
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-02 16:27:49 +0530

    Add project files.

---
+++ PackagesManagementBlazor/Server/Tools/EventDIExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.Reflection;
+
+namespace DDD.ApplicationLayer
+{
+    public static class EventDIExtensions
+    {
+        public static IServiceCollection AddAllQueries
+            (this IServiceCollection service, Assembly assembly)
+        {
+            var queries = assembly.GetTypes()
+                .Where(x => !x.IsAbstract && x.IsClass
+                && typeof(IQuery).IsAssignableFrom(x));
+            foreach (var query in queries)
+            {
+                var queryInterface = query.GetInterfaces()
+                    .Where(i => !i.IsGenericType && typeof(IQuery) != i &&
+                    typeof(IQuery).IsAssignableFrom(i))
+                    .SingleOrDefault();
+                if (queryInterface != null)
+                {
+                    service.AddTransient(queryInterface, query);
+                }
+            }
+            return service;
+        }
+    }
+}
